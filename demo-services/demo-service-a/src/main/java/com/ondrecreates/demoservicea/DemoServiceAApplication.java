e37909253d22dddef3af57894d6e5376d57340c7
OndreCreates <ondrecreates@gmail.com>
2026-07-02 15:24:16 +0200

    Docker: funkční Dockerfily pro backend a demo služby, oprava docker-compose

---
+++ demo-services/demo-service-a/src/main/java/com/ondrecreates/demoservicea/DemoServiceAApplication.java
@@ -0,0 +1,12 @@
+package com.ondrecreates.demoservicea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServiceAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoServiceAApplication.class, args);
+    }
+}
